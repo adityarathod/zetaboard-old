2257038d5bb35da1c0d3706f08b96c4cde1028ca
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-17 14:31:49 -0800

    Fix private key issue

--- firebase-server.ts
+++ firebase-server.ts
@@ -4,7 +4,7 @@ const firebaseAdminConfig = {
   type: process.env.FIREBASE_ADMIN_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -12,7 +12,6 @@ const firebaseAdminConfig = {
   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
 }
-console.log(JSON.stringify(firebaseAdminConfig))
 
 const serviceAccount: admin.ServiceAccount = {
   projectId: firebaseAdminConfig.project_id,
