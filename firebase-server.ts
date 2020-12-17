3425bd14f1a4fec5df8e29f79b7abe7a3eb727a9
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-17 14:14:02 -0800

    Add score api endpoint

---
+++ firebase-server.ts
@@ -0,0 +1,26 @@
+import admin from 'firebase-admin'
+
+const firebaseAdminConfig = {
+  type: process.env.FIREBASE_ADMIN_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+}
+
+const serviceAccount: admin.ServiceAccount = {
+  projectId: firebaseAdminConfig.project_id,
+  clientEmail: firebaseAdminConfig.client_email,
+  privateKey: firebaseAdminConfig.private_key,
+}
+
+if (typeof window === 'undefined' && !admin.apps.length) {
+  admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+}
+
+export default admin
---
+++ pages/api/score.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import type { NextApiRequest, NextApiResponse } from 'next'
+import firebase from '../../firebase-server'
+
+const rootResolver = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') await get(req, res)
+  else res.status(405).json({ error: 'method-not-allowed' })
+}
+
+const get = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { username } = req.query
+  if (!username) {
+    res.status(400).json({
+      error: 'missing-parameter',
+      details: 'missing required username parameter',
+    })
+    return
+  }
+  const querySnapshot = await firebase
+    .firestore()
+    .collection('rankings')
+    .where('user', '==', username)
+    .orderBy('score', 'desc')
+    .get()
+  const retrievedDocs = querySnapshot.docs.map(doc => doc.data())
+  res.setHeader('Cache-Control', 's-maxage=120, stale-while-revalidate')
+  res.status(200).json(retrievedDocs)
+}
+
+export default rootResolver
