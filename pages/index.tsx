d9e5962d79dd0c00cb20ba93203ff4cab2719355
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-16 18:06:08 -0800

    Retrieve Leaderboard data from Firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,40 @@
 import Head from 'next/head'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import fb from '../firebase'
+import Leaderboard from '../components/leaderboard/leaderboard'
+import LeaderEntry from '../interfaces/LeaderEntry'
 
 const Home: FC = () => {
+  const [rankings, setRankings] = useState<LeaderEntry[]>(null)
+
+  const getRankings = async () => {
+    const docs = await fb
+      .firestore()
+      .collection('rankings')
+      .orderBy('score', 'desc')
+      .limit(20)
+      .get()
+    const data = docs.docs.map(x => x.data())
+    setRankings(data as LeaderEntry[])
+  }
+
+  useEffect(() => {
+    getRankings()
+  }, [fb])
+
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Zetaboard</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className='max-w-7xl mx-auto my-0 p-4'>
-        <h1 className='mt-4 text-4xl text-center font-bold'>Zetaboard</h1>
+        <h1 className='mt-4 text-4xl text-center font-bold mb-16'>Zetaboard</h1>
+        <div className='max-w-3xl mx-auto my-0'>
+          {!rankings && <div className='text-center'>Loading...</div>}
+          <Leaderboard data={rankings} />
+        </div>
       </main>
     </div>
   )
