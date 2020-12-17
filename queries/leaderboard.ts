0774c19ae6a929d4403dcb5808400bfc4dc6a2df
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-17 11:38:59 -0800

    Add mock data to prevent excessive DB requests

--- queries/leaderboard.ts
+++ queries/leaderboard.ts
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react'
 import LeaderEntry from '../interfaces/LeaderEntry'
 import firebase from '../firebase'
+import mockLeaderboard from '../mock-data/leaderboard'
 
-const useLeaderboard = (): LeaderEntry[] => {
+const useLeaderboard = (mocked?: boolean): LeaderEntry[] => {
   const [leaders, setLeaders] = useState<LeaderEntry[]>([])
   const getLeaderboard = async () => {
+    if (mocked) {
+      if (process.env.NODE_ENV === 'production') console.error('Using mocked data in production!')
+      setLeaders(mockLeaderboard)
+      return
+    }
     const querySnapshot = await firebase
       .firestore()
       .collection('rankings')
@@ -12,6 +18,7 @@ const useLeaderboard = (): LeaderEntry[] => {
       .limit(20)
       .get()
     const retrievedDocs = querySnapshot.docs.map(doc => doc.data())
+    console.log(JSON.stringify(retrievedDocs))
     setLeaders(retrievedDocs as LeaderEntry[])
   }
   useEffect(() => {
